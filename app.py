0e6d7338b48809430005cff7e51aa40e7b870a93
muratozdemiree <murat.ozdemir.ee.eksi@gmail.com>
2024-09-17 14:34:55 +0300

    update

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ def create_ppt():
     # İlk slaytı ekle
     slide_layout = prs.slide_layouts[5]  # Boş slayt
     slide = prs.slides.add_slide(slide_layout)
+    prs.slide_width = Inches(13.33)  # 16:9 formatı için genişlik
+    prs.slide_height = Inches(7.5) 
 
     # Arkaplan resmi ekleme
     img_path_bg = os.path.join(os.getcwd(), 'slideassets', 'background1.png')
@@ -64,6 +66,65 @@ def create_ppt():
         'file_name': 'presentation.pptx',
         'file_content': pptx_base64
     })
+
+
+@app.route('/test', methods=['GET'])
+def create_ppt():
+    # PowerPoint sunumu oluştur
+   
+
+
+    prs = Presentation()
+
+    # İlk slaytı ekle
+    slide_layout = prs.slide_layouts[5]  # Boş slayt
+    slide = prs.slides.add_slide(slide_layout)
+    prs.slide_width = Inches(13.33)  # 16:9 formatı için genişlik
+    prs.slide_height = Inches(7.5) 
+
+    # Arkaplan resmi ekleme
+    img_path_bg = os.path.join(os.getcwd(), 'slideassets', 'background1.png')
+    slide.shapes.add_picture(img_path_bg, Inches(0), Inches(0), width=Inches(13.3), height=Inches(7.5))
+
+    # Diğer resmi ekleme
+    img_path = os.path.join(os.getcwd(), 'slideassets', 'image1.jpg')
+    slide.shapes.add_picture(img_path, Inches(7.61), Inches(1.40), width=Inches(4.66), height=Inches(4.67))
+
+    # Başlık ekleme
+    left = Inches(0.75)
+    top = Inches(1.67)
+    width = Inches(12)
+    height = Inches(1.5)
+    textbox = slide.shapes.add_textbox(left, top, width, height)
+    text_frame = textbox.text_frame
+    p = text_frame.add_paragraph()
+    p.text = "Client Name:\nValue Board Pack template"
+    p.font.size = Inches(0.64)  # Yaklaşık 48 pt
+    p.font.bold = True
+
+    # Başlık ekleme
+    left = Inches(0.75)
+    top = Inches(4.36)
+    width = Inches(5.78)
+    height = Inches(0.68)
+    textbox = slide.shapes.add_textbox(left, top, width, height)
+    text_frame = textbox.text_frame
+    p = text_frame.add_paragraph()
+    p.text = "Financials"
+    p.font.size = Inches(0.28)  # Yaklaşık 48 pt
+   
+
+    # Dosyayı belleğe kaydet
+    pptx_io = io.BytesIO()
+    prs.save(pptx_io)
+    pptx_io.seek(0)
+
+    # Dosyayı base64 formatında encode et
+    pptx_base64 = base64.b64encode(pptx_io.read()).decode('utf-8')
+
+    # JSON formatında base64 ile encode edilmiş dosyayı döndür
+    return send_file(pptx_file, as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
