8268eee7b8c6f23e4d45e6539ae4854f2bbdba99
muratozdemiree <murat.ozdemir.ee.eksi@gmail.com>
2024-09-23 14:40:00 +0300

    update

--- configgenerator.py
+++ configgenerator.py
@@ -1,90 +1,57 @@
 import zipfile
 import os
-import xml.etree.ElementTree as ET
-import requests
-import shutil
 
-def modify_slide_xml_and_image(zip_path, output_pptx_path):
-    # Geçici çalışma dizinini oluştur
-    temp_dir = 'temp_pptx'
+def update_xml_text_in_zip(zip_path, xml_path):
+    # Geçici dosya için bir dizin oluştur
+    temp_dir = 'temp_dir'
     os.makedirs(temp_dir, exist_ok=True)
 
-    # .pptx dosyasını aç ve dosyaları çıkar
+    # ZIP dosyasını aç ve içeriğini çıkar
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(temp_dir)
 
-    # slides klasöründeki tüm slide XML dosyalarını bul ve işle
-    slides_dir = os.path.join(temp_dir, 'ppt', 'slides')
-    slide_files = [f for f in os.listdir(slides_dir) if f.startswith('slide') and f.endswith('.xml')]
-    
+    # XML dosyasını metin olarak oku
+    xml_file_path = os.path.join(temp_dir, xml_path)
+    with open(xml_file_path, 'r', encoding='utf-8') as xml_file:
+        xml_content = xml_file.read()
 
-    # Her bir slide dosyasını işle
-    for slide_file in slide_files:
-        slide_xml_path = os.path.join(slides_dir, slide_file)
-        
-        # XML dosyasını parse et
-        tree = ET.parse(slide_xml_path)
-        root = tree.getroot()
+    # Metin olarak düzenleme (örneğin <c:numCache> ekleme)
+    new_content = xml_content.replace('<c:numCache></c:numCache>', '''
+<c:numCache>
+    <c:formatCode>_(* #,##0_);_(* \\(#,##0\\);_(* "-"??_);_(@_)</c:formatCode>
+    <c:ptCount val="14"/>
+    <c:pt idx="0"><c:v>33000</c:v></c:pt>
+    <c:pt idx="1"><c:v>66430.547826086971</c:v></c:pt>
+    <c:pt idx="2"><c:v>29400</c:v></c:pt>
+    <c:pt idx="3"><c:v>80665.665217391332</c:v></c:pt>
+    <c:pt idx="4"><c:v>30870</c:v></c:pt>
+    <c:pt idx="5"><c:v>94900.782608695677</c:v></c:pt>
+    <c:pt idx="6"><c:v>32413.5</c:v></c:pt>
+    <c:pt idx="7"><c:v>94900.782608695677</c:v></c:pt>
+    <c:pt idx="8"><c:v>34034.175000000003</c:v></c:pt>
+    <c:pt idx="9"><c:v>94900.782608695677</c:v></c:pt>
+</c:numCache>
+''')
 
-        # XML namespace tanımı (değiştirebilir)
-        namespace = {'a': 'http://schemas.openxmlformats.org/drawingml/2006/main'}
-
-        # XML içeriğinde £XX,000 ifadelerini sırayla değiştir
-        for elem in root.findall('.//a:t', namespace):
-            if 'valclient' in elem.text:
-                elem.text = elem.text.replace('valclient', 'test')
-            if 'itfinance' in elem.text:
-                elem.text = elem.text.replace('itfinance', '34,340')
-            if 'rpo' in elem.text:
-                elem.text = elem.text.replace('rpo', '34,340')
-            if 'poa' in elem.text:
-                elem.text = elem.text.replace('poa', '34,340')
-            if 'cip' in elem.text:
-                elem.text = elem.text.replace('cip', '34,340')
-            if 'mspi' in elem.text:
-                elem.text = elem.text.replace('mspi', '34,340')
-            if 'valmsl' in elem.text:
-                elem.text = elem.text.replace('valmsl', '34,340')
-            if 'valfqmr' in elem.text:
-                elem.text = elem.text.replace('valfqmr', '34,340')
-            if 'valdcap' in elem.text:
-                elem.text = elem.text.replace('valdcap', '34,340')
-            if 'valcifw' in elem.text:
-                elem.text = elem.text.replace('valcifw', '34,340')
-            if 'valoem' in elem.text:
-                elem.text = elem.text.replace('valoem', '34,340')
-            if 'valbnft' in elem.text:
-                elem.text = elem.text.replace('valbnft', '£34,340')
-            if '£valnpvv' in elem.text:
-                elem.text = elem.text.replace('£valnpvv', '£34,340')
-            if 'valacd' in elem.text:
-                elem.text = elem.text.replace('valacd', '34,340')
-            if 'valroi' in elem.text:
-                elem.text = elem.text.replace('valroi', '34')
-            if 'valinvestment' in elem.text:
-                elem.text = elem.text.replace('valinvestment', '34,340')
-            if 'valmonths' in elem.text:
-                elem.text = elem.text.replace('valmonths', '2')
-
-        # Güncellenmiş slide XML dosyasını kaydet
-        tree.write(slide_xml_path, xml_declaration=True, encoding='UTF-8')
-
-
-    
+    # Güncellenmiş içeriği tekrar dosyaya yaz
+    with open(xml_file_path, 'w', encoding='utf-8') as xml_file:
+        xml_file.write(new_content)
 
     # Güncellenmiş dosyaları tekrar ZIP yap
-    with zipfile.ZipFile(output_pptx_path, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
         for foldername, subfolders, filenames in os.walk(temp_dir):
             for filename in filenames:
                 filepath = os.path.join(foldername, filename)
                 arcname = os.path.relpath(filepath, temp_dir)
                 zip_ref.write(filepath, arcname)
 
-    # Geçici çalışma dizinini temizle
-    shutil.rmtree(temp_dir)
+    # Geçici dizini temizle
+    for foldername, subfolders, filenames in os.walk(temp_dir, topdown=False):
+        for filename in filenames:
+            os.remove(os.path.join(foldername, filename))
+        os.rmdir(foldername)
 
 # Kullanım
-zip_path = r"template.zip"  # Tam dosya yolunu girin
-output_pptx_path = r"output.pptx"  # Çıkış dosyasının yolunu belirtin
-
-modify_slide_xml_and_image(zip_path, output_pptx_path)
+zip_path = 'template.zip'
+xml_path = 'ppt/charts/chart1.xml'  # Güncellenecek XML dosyasının yolu
+update_xml_text_in_zip(zip_path, xml_path)
