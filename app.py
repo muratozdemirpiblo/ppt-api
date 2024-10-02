d49f52124d5406efd447143878b2ddbe38469883
muratozdemiree <murat.ozdemir.ee.eksi@gmail.com>
2024-10-02 18:00:01 +0300

    update

--- app.py
+++ app.py
@@ -197,8 +197,8 @@ def update_zip_with_new_xml_client(zip_path, output_zip_path, year1invest, year1
     shutil.rmtree(temp_dir)
 
 
-def update_zip_with_new_xml_questionare(zip_path, output_zip_path, year1invest, year1return, year2invest, year2return,
-                             year3invest, year3return, year4invest, year4return, year5invest, year5return,donutit,donutrpo,donutpoa,
+def update_zip_with_new_xml_questionare(zip_path, output_zip_path, baitval,barpoval,bapoaval,bacipval,bamspival,bamslval,bafqmrval,badcapval,
+                               bacifwval,baoemval,batotalval,donutit,donutrpo,donutpoa,
                              donutdcap,donutcip,donutmspi,donutmsl,donutfqmr,donutcifw,donutoem):
     temp_dir = 'temp_zip'
     os.makedirs(temp_dir, exist_ok=True)
@@ -627,24 +627,14 @@ def modify_slide_xml_and_image_client(zip_path, output_pptx_path,client_name,
     shutil.rmtree(temp_dir)
 
 def modify_slide_xml_and_image_questionare(zip_path, output_pptx_path,client_name,
-                               itfinance='0',rpo='0',poa='0',cip='0',mspi='0',valmsl='0',valfqmr='0',valdcap='0',
-                               valcifw='0',valoem='0',valbnft='0',valnpvv='0',valacd='0',valroi='0',valinvestment='0',valmonths='0',valhours='0',
-                               year1invest='0',
-                                year1return='0',year2invest='0',year2return='0',year3invest='0',
-                                year3return='0',year4invest='0',year4return='0',year5invest='0',
-                                year5return='0',costofdoingnothing1='0',itfinanceper='0',rpoper='0',poaper='0',cipper='0'
-                                ,mspiper='0',valmslper='0',valfqmrper='0',valdcapper='0',
-                               valcifwper='0',valoemper='0',
+                               baitval='0',barpoval='0',bapoaval='0',bacipval='0',bamspival='0',bamslval='0',bafqmrval='0',badcapval='0',
+                               bacifwval='0',baoemval='0',batotalval='0',
                                donutit='0',donutrpo='0',donutpoa='0',donutdcap='0',donutcip='0',donutmspi='0',donutmsl='0',
-                               donutfqmr='0',donutcifw='0',donutoem='0',
-                               prpoval='0',ppoaval='0',pcipval='0',pmspival='0',pmslval='0',pfqmrval='0',
-                                pdcapval='0',pcifwval='0',poemval='0',pitfinanceval='0',totalcostval='0',per1x='0',per2x='0',
-                                per3x='0',per4x='0',per5x='0',per6x='0',per7x='0',per8x='0',per9x='0',per10x='0'):
+                               donutfqmr='0',donutcifw='0',donutoem='0'):
     # Geçici çalışma dizinini oluştur
     temp_dir = 'questionare_temp_pptx'
     os.makedirs(temp_dir, exist_ok=True)
 
-    print(pitfinanceval)
 
     # .pptx dosyasını aç ve dosyaları çıkar
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
@@ -666,120 +656,39 @@ def modify_slide_xml_and_image_questionare(zip_path, output_pptx_path,client_nam
         # XML namespace tanımı (değiştirebilir)
         namespace = {'a': 'http://schemas.openxmlformats.org/drawingml/2006/main'}
 
-        donutpercentvals = ''''''
-        if rpoper!='0' and rpoper:
-            donutpercentvals+='\nRaising Purchase Orders: We anticipate a {}% efficiency'.format(rpoper)
-        if itfinanceper!='0' and itfinanceper:
-            donutpercentvals+='\nIT finance systems: We anticipate a {}% efficiency'.format(itfinanceper)
-        if poaper!='0' and poaper:
-            donutpercentvals+='\nPurchase Order Approvals: We anticipate a {}% efficiency'.format(poaper)
-        if cipper!='0' and cipper:
-            donutpercentvals+='\nCoding invoice processes: We anticipate a {}% efficiency'.format(cipper)
-        if mspiper!='0' and mspiper:
-            donutpercentvals+='\nManagement of Supplier and Purchase Invoices: We anticipate a {}% efficiency'.format(mspiper)
-        if valmslper!='0' and valmslper:
-            donutpercentvals+='\nManaging Maverick Spend & Spend Leakage: We anticipate a {}% efficiency'.format(valmslper)
-        if valfqmrper!='0' and valfqmrper:
-            donutpercentvals+='\nFinance Query Management and Dashboard Reporting: We anticipate a {}% efficiency'.format(valfqmrper)
-        if valdcapper!='0' and valdcapper:
-            donutpercentvals+='\nDebt Collection Administration Processes: We anticipate a {}% efficiency'.format(valdcapper)
-        if valcifwper!='0' and valcifwper:
-            donutpercentvals+='\nCustomer Invoicing & Finance Workflow Management: We anticipate a {}% efficiency'.format(valcifwper)
-        
-        for elem in root.findall('.//a:t', namespace):
-            if 'per1x' in elem.text:
-                elem.text = elem.text.replace('per1x', str(per1x))
-            if 'per2x' in elem.text:
-                elem.text = elem.text.replace('per2x', str(per2x))
-            if 'per3x' in elem.text:
-                elem.text = elem.text.replace('per3x', str(per3x))
-            if 'per4x' in elem.text:
-                elem.text = elem.text.replace('per4x', str(per4x))
-            if 'per5x' in elem.text:
-                elem.text = elem.text.replace('per5x', str(per5x))
-            if 'per6x' in elem.text:
-                elem.text = elem.text.replace('per6x', str(per6x))
-            if 'per7x' in elem.text:
-                elem.text = elem.text.replace('per7x', str(per7x))
-            if 'per8x' in elem.text:
-                elem.text = elem.text.replace('per8x', str(per8x))
-            if 'per9x' in elem.text:
-                elem.text = elem.text.replace('per9x', str(per9x))
-            if 'per10x' in elem.text:
-                elem.text = elem.text.replace('per10x', str(per10x))
-
-            if 'prpoval' in elem.text:
-                print(prpoval)
-                elem.text = elem.text.replace('prpoval', prpoval.replace('£',''))
-            if 'ppoaval' in elem.text:
-                elem.text = elem.text.replace('ppoaval', ppoaval.replace('£',''))
-            if 'pcipval' in elem.text:
-                elem.text = elem.text.replace('pcipval', pcipval.replace('£',''))
-            if 'pmspival' in elem.text:
-                elem.text = elem.text.replace('pmspival', pmspival.replace('£',''))
-            if 'pmslval' in elem.text:
-                elem.text = elem.text.replace('pmslval', pmslval.replace('£',''))
-            if 'pfqmrval' in elem.text:
-                elem.text = elem.text.replace('pfqmrval', pfqmrval.replace('£',''))
-            if 'pdcapval' in elem.text:
-                elem.text = elem.text.replace('pdcapval', pdcapval.replace('£',''))
-            if 'pcifwval' in elem.text:
-                elem.text = elem.text.replace('pcifwval', pcifwval.replace('£',''))
-            if 'poemval' in elem.text:
-                elem.text = elem.text.replace('poemval', poemval.replace('£',''))
-            if 'pitfinanceval' in elem.text:
-                elem.text = elem.text.replace('pitfinanceval', pitfinanceval.replace('£',''))
-        # XML içeriğinde £XX,000 ifadelerini sırayla değiştir
+         
         for elem in root.findall('.//a:t', namespace):
             if 'valclient' in elem.text:
                 elem.text = elem.text.replace('valclient', client_name)
-            if 'itfinance' in elem.text:
-                elem.text = elem.text.replace('itfinance', format_with_commas(itfinance.replace('£','')))
-            if 'valrpo' in elem.text:
-                elem.text = elem.text.replace('valrpo', format_with_commas(rpo.replace('£','')))
-            if 'valpoa' in elem.text:
-                elem.text = elem.text.replace('valpoa', format_with_commas(poa.replace('£','')))
-            if 'valcip' in elem.text:
-                elem.text = elem.text.replace('valcip', format_with_commas(cip.replace('£','')))
-            if 'valmspi' in elem.text:
-                elem.text = elem.text.replace('valmspi', format_with_commas(mspi.replace('£','')))
-            if 'valmsl' in elem.text:
-                elem.text = elem.text.replace('valmsl', format_with_commas(valmsl.replace('£','')))
-            if 'valfqmr' in elem.text:
-                elem.text = elem.text.replace('valfqmr', format_with_commas(valfqmr.replace('£','')))
-            if 'valdcap' in elem.text:
-                elem.text = elem.text.replace('valdcap', format_with_commas(valdcap.replace('£','')))
-            if 'valcifw' in elem.text:
-                elem.text = elem.text.replace('valcifw', format_with_commas(valcifw.replace('£','')))
-            if 'valoem' in elem.text:
-                elem.text = elem.text.replace('valoem', format_with_commas(valoem.replace('£','')))
-            if 'valbnft' in elem.text:
-                elem.text = elem.text.replace('valbnft', format_with_commas(valbnft.replace('£','')))
-            if 'valnpvv' in elem.text:
-                elem.text = elem.text.replace('valnpvv', format_with_commas(valnpvv.replace('£','')))
-            if 'valacd' in elem.text:
-                elem.text = elem.text.replace('valacd', format_with_commas(valacd.replace('£','')))
-            if 'valroi' in elem.text:
-                elem.text = elem.text.replace('valroi', format_with_commas(valroi.replace('£','')))
-            if 'valinvestment' in elem.text:
-                elem.text = elem.text.replace('valinvestment', format_with_commas(valinvestment.replace('£','')))
-            if 'valmonths' in elem.text:
-                elem.text = elem.text.replace('valmonths', valmonths)
-            if 'valhours' in elem.text:
-                valhours+='h'
-                elem.text = elem.text.replace('valhours', valhours)
-            if 'valcostof' in elem.text:
-                elem.text = elem.text.replace('valcostof', costofdoingnothing1.replace('£',''))
-            if 'valdonutpercentvalues' in elem.text:
-                elem.text = elem.text.replace('valdonutpercentvalues',donutpercentvals)
+            if 'baitval' in elem.text:
+                elem.text = elem.text.replace('baitval', format_with_commas(baitval.replace('£','')))
+            if 'barpoval' in elem.text:
+                elem.text = elem.text.replace('barpoval', format_with_commas(barpoval.replace('£','')))
+            if 'bapoaval' in elem.text:
+                elem.text = elem.text.replace('bapoaval', format_with_commas(bapoaval.replace('£','')))
+            if 'bacipval' in elem.text:
+                elem.text = elem.text.replace('bacipval', format_with_commas(bacipval.replace('£','')))
+            if 'bamspival' in elem.text:
+                elem.text = elem.text.replace('bamspival', format_with_commas(bamspival.replace('£','')))
+            if 'bamslval' in elem.text:
+                elem.text = elem.text.replace('bamslval', format_with_commas(bamslval.replace('£','')))
+            if 'bafqmrval' in elem.text:
+                elem.text = elem.text.replace('bafqmrval', format_with_commas(bafqmrval.replace('£','')))
+            if 'badcapval' in elem.text:
+                elem.text = elem.text.replace('badcapval', format_with_commas(badcapval.replace('£','')))
+            if 'bacifwval' in elem.text:
+                elem.text = elem.text.replace('bacifwval', format_with_commas(bacifwval.replace('£','')))
+            if 'baoemval' in elem.text:
+                elem.text = elem.text.replace('baoemval', format_with_commas(baoemval.replace('£','')))
+            if 'batotalval' in elem.text:
+                elem.text = elem.text.replace('batotalval', format_with_commas(batotalval.replace('£','')))
+            
         zip_path = 'questionare_template.zip'  # Güncellemek istediğin template.zip
         output_zip_path = 'questionare_template.zip'  # Çıkış dosyasının adı
 
         # Yeni XML dosyasını oluştur ve ZIP dosyasını güncelle
-        update_zip_with_new_xml_questionare(zip_path, output_zip_path,year1invest=year1invest,
-                                year1return=year1return,year2invest=year2invest,year2return=year2return,year3invest=year3invest,
-                                year3return=year3return,year4invest=year4invest,year4return=year4return,year5invest=year5invest,
-                                year5return=year5return,donutit=donutit,donutrpo=donutrpo,donutpoa=donutpoa,donutdcap=donutdcap,
+        update_zip_with_new_xml_questionare(zip_path, output_zip_path,baitval='0',barpoval='0',bapoaval='0',bacipval='0',bamspival='0',bamslval='0',bafqmrval='0',badcapval='0',
+                               bacifwval='0',baoemval='0',batotalval='0',donutit=donutit,donutrpo=donutrpo,donutpoa=donutpoa,donutdcap=donutdcap,
                                 donutcip=donutcip,donutmspi=donutmspi,donutmsl=donutmsl,donutfqmr=donutfqmr,donutcifw=donutcifw,
                                 donutoem=donutoem)
 
@@ -1147,49 +1056,17 @@ def create_questionare_ppt():
     # 'client_name' parametresini POST isteği ile al
     data = request.get_json()
     client_name = data.get('client_name') or ""
-    itfinance = data.get('itfinance') or ""
-    rpo = data.get('rpo') or ""
-    poa = data.get('poa') or ""
-    cip = data.get('cip') or ""
-    mspi = data.get('mspi') or ""
-    valmsl = data.get('valmsl') or ""
-    valfqmr = data.get('valfqmr') or ""
-    valdcap = data.get('valdcap') or ""
-    valcifw = data.get('valcifw') or ""
-    valoem = data.get('valoem') or ""
-    valbnft = data.get('valbnft') or ""
-    valnpvv = data.get('valnpvv') or ""
-    valacd = data.get('valacd') or ""
-    valroi = data.get('valroi') or ""
-    valinvestment = data.get('valinvestment') or ""
-    valmonths = data.get('valmonths') or ""
-    valhours = data.get('valhours') or ""
-    year1total = data.get('year1total') or ""
-    year1invest = data.get('year1invest') or ""
-	
-    year2otal = data.get('year2total') or ""
-    year2invest = data.get('year2invest') or ""
-
-    year3total = data.get('year3total') or ""
-    year3invest = data.get('year3invest') or ""
-
-    year4total = data.get('year4total') or ""
-    year4invest = data.get('year4invest') or ""
-
-    year5total = data.get('year5total') or ""
-    year5invest = data.get('year5invest') or ""
-    costofdoingnothing1 = data.get('costofdoingnothing1') or ""
-
-    itfinanceper = data.get('itfinanceper') or ""
-    rpoper = data.get('rpoper') or ""
-    poaper = data.get('poaper') or ""
-    cipper = data.get('cipper') or ""
-    mspiper = data.get('mspiper') or ""
-    valmslper = data.get('valmslper') or ""
-    valfqmrper = data.get('valfqmrper') or ""
-    valdcapper = data.get('valdcapper') or ""
-    valcifwper = data.get('valcifwper') or ""
-    valoemper = data.get('valoemper') or ""
+    baitval = data.get('baitval') or "0"
+    barpoval = data.get('barpoval') or "0"
+    bapoaval = data.get('bapoaval') or "0"
+    bacipval = data.get('bacipval') or "0"
+    bamspival = data.get('bamspival') or "0"
+    bamslval = data.get('bamslval') or "0"
+    bafqmrval = data.get('bafqmrval') or "0"
+    badcapval = data.get('badcapval') or "0"
+    bacifwval = data.get('bacifwval') or "0"
+    baoemval = data.get('baoemval') or "0"
+    batotalval = data.get('batotalval') or "0"
 
     donutit = data.get('donutit') or "0"
     donutrpo = data.get('donutrpo') or "0"
@@ -1262,19 +1139,11 @@ def create_questionare_ppt():
     zip_path = r"questionare_template.zip"  # Tam dosya yolunu girin
     output_pptx_path = r"questionare_output.pptx"  # Çıkış dosyasının yolunu belirtin
 
-    modify_slide_xml_and_image_questionare(zip_path, output_pptx_path,client_name,itfinance,rpo,poa,cip,mspi,valmsl,valfqmr,valdcap,
-                               valcifw,valoem,valbnft,valnpvv,valacd,valroi,valinvestment,valmonths,valhours,
-                               year1invest=year1invest,
-                                year1return=year1total,year2invest=year2invest,year2return=year2otal,year3invest=year3invest,
-                                year3return=year3total,year4invest=year4invest,year4return=year4total,year5invest=year5invest,
-                                year5return=year5total,costofdoingnothing1=costofdoingnothing1,itfinanceper=itfinanceper,rpoper=rpoper,
-                                poaper=poaper,cipper=cipper,mspiper=mspiper,valmslper=valmslper,valfqmrper=valfqmrper,valdcapper=valdcapper,
-                                valcifwper=valcifwper,valoemper=valoemper,donutit=donutit,donutrpo=donutrpo,donutpoa=donutpoa,
-                                donutdcap=donutdcap,donutcip=donutcip,donutmspi=donutmspi,donutmsl=donutmsl,donutfqmr=donutfqmr,
-                                donutcifw=donutcifw,donutoem=donutoem,
-                                prpoval=prpoval,ppoaval=ppoaval,pcipval=pcipval,pmspival=pmspival,pmslval=pmslval,pfqmrval=pfqmrval,
-                                pdcapval=pdcapval,pcifwval=pcifwval,poemval=poemval,pitfinanceval=pitfinanceval,totalcostval=totalcostval,
-                                per1x=per1x,per2x=per2x,per3x=per3x,per4x=per4x,per5x=per5x,per6x=per6x,per7x=per7x,per8x=per8x,per9x=per9x,per10x=per10x)
+    modify_slide_xml_and_image_questionare(zip_path, output_pptx_path,client_name,
+                                           baitval=baitval,barpoval=barpoval,bapoaval=bapoaval,bacipval=bacipval,bamspival=bamspival,bamslval=bamslval,bafqmrval=bafqmrval,badcapval=badcapval,
+                               bacifwval=bacifwval,baoemval=baoemval,batotalval=batotalval,
+                               donutit=donutit,donutrpo=donutrpo,donutpoa=donutpoa,donutdcap=donutdcap,donutcip=donutcip,donutmspi=donutmspi,donutmsl=donutmsl,
+                               donutfqmr=donutfqmr,donutcifw=donutcifw,donutoem=donutoem)
     
     zip_path = 'questionare_template.zip'  # Güncellemek istediğin template.zip
     output_zip_path = 'questionare_template.zip'  # Çıkış dosyasının adı
