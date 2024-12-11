53a522fad16c9dfac8762a629599bf12f9c885b2
muratozdemiree <murat.ozdemir.ee.eksi@gmail.com>
2024-12-11 15:39:38 +0300

    update

--- app.py
+++ app.py
@@ -329,7 +329,7 @@ def modify_slide_xml_and_image(zip_path, output_pptx_path,client_name,
                                 eytimecifw='0',eytimeoem='0',eytimeitfinance='0',q1itfinance='',q1rpo='',q2rpo='',q3rpo='',q1poa='',q2poa='',q3poa='',q1cip='',
     q2cip='',q3cip='',q4cip='',q1mspi='',q2mspi='',q3mspi='',q1msl='',q2msl='',q1fqmr='',
     q2fqmr='',q3fqmr='',q4fqmr='',q1dcap='',q2dcap='',q3dcap='',q1cifw='',q2cifw='',
-    q0itfinance='',q0rpo='',q0poa='',q0mspi='',q0msl='',q0fqmr='',q0dcap='',q0cifw='',valsubmissionid=''):
+    q0itfinance='hosted',q0rpo='this is a gap today',q0poa='this is a gap today',q0mspi='manual entry',q0msl='this is a gap today',q0fqmr='this is a gap today',q0dcap='this is a gap today',q0cifw='this is a gap today',valsubmissionid=''):
     
 
     # Geçici çalışma dizinini oluştur
